1ad44db53a346a8ef680221838a07bca1be5d859
Daniel Lin <shiqi.lin.8661@gmail.com>
2021-03-07 14:17:05 +1100

    now using map instead of hardcoding the search result cards, also refactored the garage pictures to be squares

--- src/parkearn/ParkEarnMap.js
+++ src/parkearn/ParkEarnMap.js
@@ -13,8 +13,9 @@ import CardFooter from "../lib/controls/containers/card/CardFooter";
 import CardImage from "../lib/controls/containers/card/CardImage";
 import CardSubtitle from "../lib/controls/containers/card/CardSubtitle";
 import CardTitle from "../lib/controls/containers/card/CardTitle";
-import garagePic1 from "./garage.jpg";
-import garagePic2 from "./garage2.jpg";
+import garagePic1 from "./garage1.jpg";
+import garagePic2 from "./garage3.jpg";
+import garagePic3 from "./garage5.jpg";
 
 import ParkEarnBooking from "./ParkEarnBooking";
 
@@ -23,7 +24,29 @@ class ParkEarnMap extends Component {
   constructor({}) {
     super({});
   }
-  state = { clicked: false };
+  state = {
+    clicked: false,
+    parkingList: [
+      {
+        address: "5 Murphy Street, Kew VIC 3101",
+        title: "Driveway",
+        rate: "$3.5/hr",
+        picture: garagePic1,
+      },
+      {
+        address: "2 Murphy Street, Kew VIC 3101",
+        title: "Clean and spacious driveway",
+        rate: "$4/hr",
+        picture: garagePic2,
+      },
+      {
+        address: "99 Stevenson Street, Kew VIC 3101",
+        title: "Park in garage",
+        rate: "$3.7/hr",
+        picture: garagePic3,
+      },
+    ],
+  };
 
   render() {
     return (
@@ -67,61 +90,43 @@ class ParkEarnMap extends Component {
                       </button>
                     </Form>
                   </Formik>
-                  <p>
-                    <Button
-                      onClick={() => {
-                        this.setState({ clicked: true });
-                      }}
-                      style={{ height: "180px", width: "400px" }}
-                    >
-                      <Card>
-                        <FlexRow>
-                          <FlexCol>
-                            <CardImage>
-                              <img
-                                src={garagePic1}
-                                style={{
-                                  height: "150px",
-                                  width: "100%",
-                                }}
-                                resizeMode={"contain"}
-                              />
-                            </CardImage>
-                          </FlexCol>
-                          <FlexCol>
-                            <CardHeader>
-                              <CardTitle>123 High Street</CardTitle>
-                            </CardHeader>
-                            <CardBody>12:00pm to 6:00pm</CardBody>
-                          </FlexCol>
-                        </FlexRow>
-                      </Card>
-                    </Button>
-                    <Button style={{ height: "180px", width: "400px" }}>
-                      <Card>
-                        <FlexRow>
-                          <FlexCol>
-                            <CardImage>
-                              <img
-                                src={garagePic2}
-                                style={{
-                                  height: "150px",
-                                  width: "100%",
-                                }}
-                                resizeMode={"contain"}
-                              />
-                            </CardImage>
-                          </FlexCol>
-                          <FlexCol>
-                            <CardHeader>
-                              <CardTitle>97 Springvale Road</CardTitle>
-                            </CardHeader>
-                            <CardBody>11:00pm to 6:00pm</CardBody>
-                          </FlexCol>
-                        </FlexRow>
-                      </Card>
-                    </Button>
-                  </p>
+
+                  {this.state.parkingList.map((item) => (
+                    <p>
+                      <Button
+                        onClick={() => {
+                          this.setState({ clicked: true });
+                        }}
+                        style={{ height: "100%", width: "100%" }}
+                      >
+                        <Card>
+                          <FlexRow singleLine={true}>
+                            <FlexCol>
+                              <CardImage>
+                                <img
+                                  src={item.picture}
+                                  style={{
+                                    height: "100%",
+                                    width: "100%",
+                                  }}
+                                  resizeMode={"contain"}
+                                />
+                              </CardImage>
+                            </FlexCol>
+                            <FlexCol className="cardTextField">
+                              <CardHeader>
+                                <CardTitle singleLine={false}>
+                                  {item.title}
+                                </CardTitle>
+                                <CardSubtitle>{item.address}</CardSubtitle>
+                              </CardHeader>
+                              <CardBody>{item.rate}</CardBody>
+                            </FlexCol>
+                          </FlexRow>
+                        </Card>
+                      </Button>
+                    </p>
+                  ))}
                 </FlexCol>
                 <FlexCol className="halves" defaultSize={3}>
                   map on the right-hand side
